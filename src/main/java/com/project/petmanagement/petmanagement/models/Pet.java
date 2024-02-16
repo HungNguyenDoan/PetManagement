2beeba3af72fb8252fa8278da4c8f575d3879f45
DangKhai832 <dangkhai832>
2024-02-16 16:05:42 +0700

    update pet

--- src/main/java/com/project/petmanagement/petmanagement/models/Pet.java
+++ src/main/java/com/project/petmanagement/petmanagement/models/Pet.java
@@ -1,30 +1,35 @@
 package com.project.petmanagement.petmanagement.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Entity
-@Table(name = "pet")
+@Table(name = "pets")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "userId")
-    private Long userId;
-    @Column(name = "full_name")
-    private String fullName;
-    @Column(name = "date_of_Birth")
+    @Column(name = "fullname", nullable = false)
+    private String fullname;
+    @Column(name = "user_id")
+    private String userId;
+    @DateTimeFormat
     private Date dateOfBirth;
-    @Column(name = "species_id")
+    @Column(name = "species_id",nullable = false)
     private Long speciesId;
     @Column(name = "breed")
     private String breed;
