507933483d94a635e20f86ceeec93e1ec934e591
manh1357902 <manh7a2002@gmail.com>
2024-03-14 10:43:59 +0700

    update

--- app/src/main/java/com/project/petmanagement/fragment/ShopCartFragment.java
+++ app/src/main/java/com/project/petmanagement/fragment/ShopCartFragment.java
@@ -2,7 +2,10 @@ package com.project.petmanagement.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,20 +15,19 @@ import com.project.petmanagement.R;
 
 public class ShopCartFragment extends Fragment {
 
-
+    private RecyclerView carItemRecyclerView;
 
     public ShopCartFragment() {
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_shop_cart, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
