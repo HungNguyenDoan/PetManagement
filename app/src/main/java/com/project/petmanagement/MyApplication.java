a4c0561cb27c60be8d62fc0b2948b53bec33fa54
manh1357902 <manh7a2002@gmail.com>
2024-04-01 12:09:45 +0700

    update

--- app/src/main/java/com/project/petmanagement/MyApplication.java
+++ app/src/main/java/com/project/petmanagement/MyApplication.java
@@ -3,7 +3,13 @@ package com.project.petmanagement;
 import android.app.Application;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
+import com.project.petmanagement.payload.response.ListPetResponse;
+import com.project.petmanagement.services.ApiService;
 import com.project.petmanagement.services.StorageService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MyApplication extends Application {
     private static StorageService storageService;
@@ -12,6 +18,21 @@ public class MyApplication extends Application {
         super.onCreate();
         AndroidThreeTen.init(this);
         storageService = new StorageService(getApplicationContext());
+        ApiService.apiService.getAllPetUser().enqueue(new Callback<ListPetResponse>() {
+            @Override
+            public void onResponse(Call<ListPetResponse> call, Response<ListPetResponse> response) {
+                if(response.code() == 403){
+                    storageService.remove("token");
+                    storageService.remove("user");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ListPetResponse> call, Throwable t) {
+                storageService.remove("token");
+                storageService.remove("user");
+            }
+        });
     }
     public static StorageService getStorageService(){
         return storageService;
