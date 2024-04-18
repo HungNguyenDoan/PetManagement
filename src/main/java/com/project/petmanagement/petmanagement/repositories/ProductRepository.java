e61c05f0b869b6fce4d76c3b178cbf3151274a74
Kien Do <106758035+ddki1912@users.noreply.github.com>
2024-04-18 22:54:33 +0700

    update ProductRepository

--- src/main/java/com/project/petmanagement/petmanagement/repositories/ProductRepository.java
+++ src/main/java/com/project/petmanagement/petmanagement/repositories/ProductRepository.java
@@ -2,19 +2,8 @@ package com.project.petmanagement.petmanagement.repositories;
 
 import com.project.petmanagement.petmanagement.models.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query(value = "select p from products p " +
-            "left join categories_products cp on p.id = cp.product_id " +
-            "left join categories c on c.id = cp.category_id " +
-            "where c.id = :categoryId", nativeQuery = true)
-    List<Product> findByCategory(@Param("categoryId") Long categoryId);
-
-    List<Product> findByNameContainingIgnoreCase(String name);
 }
