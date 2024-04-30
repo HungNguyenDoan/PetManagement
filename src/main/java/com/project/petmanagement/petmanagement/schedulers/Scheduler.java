42e814eca1d0e21e2f6bf1ebf391a95085e3aa98
Kien Do <106758035+ddki1912@users.noreply.github.com>
2024-04-30 22:28:13 +0700

    update FrequencyEnum and Scheduler

--- src/main/java/com/project/petmanagement/petmanagement/schedulers/Scheduler.java
+++ src/main/java/com/project/petmanagement/petmanagement/schedulers/Scheduler.java
@@ -14,10 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.Month;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -29,6 +26,14 @@ public class Scheduler {
     private final VaccinationNotificationService vaccinationNotificationService;
     private final CareActivityNotificationService careActivityNotificationService;
     private final FirebaseService firebaseService;
+
+    private LocalDate isValid(int year, int month, int day) {
+        try {
+            return LocalDate.of(year, month, day);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
     @Scheduled(cron = "0 0 6 * * *")
     @Transactional
@@ -107,73 +112,35 @@ public class Scheduler {
                             }
                         } else if (frequency.compareTo(FrequencyEnum.WEEKLY) == 0) {
                             List<DayOfWeek> scheduledDaysOfWeek = recurringSchedule.getDaysOfWeek();
-                            Collections.sort(scheduledDaysOfWeek);
                             LocalDate fromDate = recurringSchedule.getFromDate().toLocalDate();
                             LocalDate toDate = recurringSchedule.getToDate().toLocalDate();
                             DayOfWeek dateToDayOfWeek = currentDate.getDayOfWeek();
                             if (scheduledDaysOfWeek.contains(dateToDayOfWeek)) {
-                                List<LocalDate> dateList = new ArrayList<>();
-                                LocalDate firstDayOfWeek = fromDate.with(TemporalAdjusters.nextOrSame(scheduledDaysOfWeek.get(0)));
-                                dateList.add(firstDayOfWeek);
-                                for (int i = 1; i < scheduledDaysOfWeek.size(); i++) {
-                                    dateList.add(firstDayOfWeek.with(TemporalAdjusters.nextOrSame(scheduledDaysOfWeek.get(i))));
-                                }
+                                LocalDate firstDayOfWeek = fromDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+                                LocalDate lastDayOfWeek = fromDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
                                 boolean stop = false;
-                                for (LocalDate localDate : dateList) {
+                                while (!firstDayOfWeek.isAfter(lastDayOfWeek)) {
                                     if (stop) {
                                         break;
                                     }
-                                    LocalDate tempDate = localDate;
-                                    while (!tempDate.isAfter(toDate)) {
-                                        log.info("WEEKLY: " + tempDate);
-                                        if (tempDate.equals(currentDate) && scheduledTime.equals(currentTime)) {
-                                            log.info("WEEKLY: OK");
-                                            onTime = true;
-                                            stop = true;
-                                            break;
-                                        } else if (tempDate.isAfter(currentDate)) {
-                                            break;
+                                    if (scheduledDaysOfWeek.contains(firstDayOfWeek.getDayOfWeek())) {
+                                        LocalDate tempDate = firstDayOfWeek;
+                                        while (!tempDate.isAfter(toDate)) {
+                                            if (!tempDate.isBefore(fromDate)) {
+                                                log.info("WEEKLY: " + tempDate);
+                                                if (tempDate.equals(currentDate) && scheduledTime.equals(currentTime)) {
+                                                    log.info("WEEKLY: OK");
+                                                    onTime = true;
+                                                    stop = true;
+                                                    break;
+                                                } else if (tempDate.isAfter(currentDate)) {
+                                                    break;
+                                                }
+                                            }
+                                            tempDate = tempDate.plusWeeks(step);
                                         }
-                                        tempDate = tempDate.plusWeeks(step);
                                     }
-                                }
-                            }
-                        } else if (frequency.compareTo(FrequencyEnum.MONTHLY) == 0) {
-                            List<Integer> scheduledDaysOfMonth = recurringSchedule.getDaysOfMonth();
-                            Collections.sort(scheduledDaysOfMonth);
-                            LocalDate fromDate = recurringSchedule.getFromDate().toLocalDate();
-                            LocalDate toDate = recurringSchedule.getToDate().toLocalDate();
-                            int dayOfMonth = currentDate.getDayOfMonth();
-                            if (scheduledDaysOfMonth.contains(dayOfMonth)) {
-                                List<LocalDate> dateList = new ArrayList<>();
-                                if (fromDate.getDayOfMonth() <= scheduledDaysOfMonth.get(0)) {
-                                    for (Integer i : scheduledDaysOfMonth) {
-                                        dateList.add(LocalDate.of(fromDate.getYear(), fromDate.getMonth(), i));
-                                    }
-                                } else {
-                                    for (Integer i : scheduledDaysOfMonth) {
-                                        Month nextMonth = fromDate.getMonth().plus(1);
-                                        dateList.add(LocalDate.of(!nextMonth.equals(Month.JANUARY) ? fromDate.getYear() : fromDate.getYear() + 1, nextMonth, i));
-                                    }
-                                }
-                                boolean stop = false;
-                                for (LocalDate localDate : dateList) {
-                                    if (stop) {
-                                        break;
-                                    }
-                                    LocalDate tempDate = localDate;
-                                    while (!tempDate.isAfter(toDate)) {
-                                        log.info("MONTHLY: " + tempDate);
-                                        if (tempDate.equals(currentDate) && scheduledTime.equals(currentTime)) {
-                                            log.info("MONTHLY: OK");
-                                            onTime = true;
-                                            stop = true;
-                                            break;
-                                        } else if (tempDate.isAfter(currentDate)) {
-                                            break;
-                                        }
-                                        tempDate = tempDate.plusMonths(step);
-                                    }
+                                    firstDayOfWeek = firstDayOfWeek.plusDays(1);
                                 }
                             }
                         }
