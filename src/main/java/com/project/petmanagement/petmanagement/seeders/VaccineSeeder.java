62d1372d1ff4c7c2988287445225439ffbacc1a4
HungND <hungnguyendoan.2002@gmail.com>
2024-04-30 10:25:57 +0700

    add id for vaccine seeder

--- src/main/java/com/project/petmanagement/petmanagement/seeders/VaccineSeeder.java
+++ src/main/java/com/project/petmanagement/petmanagement/seeders/VaccineSeeder.java
@@ -22,16 +22,16 @@ public class VaccineSeeder {
     public void seed() {
         List<Species> listSpecies = speciesRepository.findAll();
 
-        Vaccine v1 = Vaccine.builder().name("Vaccine Parvovirus").vaccineDose(1).species(listSpecies.get(0)).build();
-        Vaccine v2 = Vaccine.builder().name("Vaccine Distemper").vaccineDose(1).species(listSpecies.get(0)).build();
-        Vaccine v3 = Vaccine.builder().name("Vaccine Hepatitis C").vaccineDose(1).species(listSpecies.get(0)).build();
-        Vaccine v4 = Vaccine.builder().name("Vaccine Parainfluenza").vaccineDose(1).species(listSpecies.get(0)).build();
-        Vaccine v5 = Vaccine.builder().name("Vaccine Rabies").vaccineDose(1).species(listSpecies.get(0)).build();
-        Vaccine v6 = Vaccine.builder().name("Vaccine Panleukopenia").vaccineDose(1).species(listSpecies.get(1)).build();
-        Vaccine v7 = Vaccine.builder().name("Vaccine Calicivirus").vaccineDose(1).species(listSpecies.get(1)).build();
-        Vaccine v8 = Vaccine.builder().name("Vaccine Herpesvirus").vaccineDose(1).species(listSpecies.get(1)).build();
-        Vaccine v9 = Vaccine.builder().name("Vaccine Chlamydia").vaccineDose(1).species(listSpecies.get(1)).build();
-        Vaccine v10 = Vaccine.builder().name("Vaccine Rabies").vaccineDose(1).species(listSpecies.get(1)).build();
+        Vaccine v1 = Vaccine.builder().id(1L).name("Vaccine Parvovirus").vaccineDose(1).species(listSpecies.get(0)).build();
+        Vaccine v2 = Vaccine.builder().id(2L).name("Vaccine Distemper").vaccineDose(1).species(listSpecies.get(0)).build();
+        Vaccine v3 = Vaccine.builder().id(3L).name("Vaccine Hepatitis C").vaccineDose(1).species(listSpecies.get(0)).build();
+        Vaccine v4 = Vaccine.builder().id(4L).name("Vaccine Parainfluenza").vaccineDose(1).species(listSpecies.get(0)).build();
+        Vaccine v5 = Vaccine.builder().id(5L).name("Vaccine Rabies").vaccineDose(1).species(listSpecies.get(0)).build();
+        Vaccine v6 = Vaccine.builder().id(6L).name("Vaccine Panleukopenia").vaccineDose(1).species(listSpecies.get(1)).build();
+        Vaccine v7 = Vaccine.builder().id(7L).name("Vaccine Calicivirus").vaccineDose(1).species(listSpecies.get(1)).build();
+        Vaccine v8 = Vaccine.builder().id(8L).name("Vaccine Herpesvirus").vaccineDose(1).species(listSpecies.get(1)).build();
+        Vaccine v9 = Vaccine.builder().id(9L).name("Vaccine Chlamydia").vaccineDose(1).species(listSpecies.get(1)).build();
+        Vaccine v10 = Vaccine.builder().id(10L).name("Vaccine Rabies").vaccineDose(1).species(listSpecies.get(1)).build();
 
         vaccineRepository.saveAll(Arrays.asList(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10));
     }
