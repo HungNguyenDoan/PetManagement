e79e32e271e9cad06c61fd7a60688db6ee2528ab
HungND <hungnguyendoan.2002@gmail.com>
2024-05-01 17:35:58 +0700

    add querydsl dependency and update search nutritious search function

--- src/main/java/com/project/petmanagement/petmanagement/repositories/NutritiousFoodRepository.java
+++ src/main/java/com/project/petmanagement/petmanagement/repositories/NutritiousFoodRepository.java
@@ -4,12 +4,13 @@ import com.project.petmanagement.petmanagement.models.entity.FoodType;
 import com.project.petmanagement.petmanagement.models.entity.NutritiousFood;
 import com.project.petmanagement.petmanagement.models.entity.Species;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface NutritiousFoodRepository extends JpaRepository<NutritiousFood, Long> {
+public interface NutritiousFoodRepository extends JpaRepository<NutritiousFood, Long>, QuerydslPredicateExecutor<NutritiousFood> {
     List<NutritiousFood> findBySpecies(Species species);
 
     List<NutritiousFood> findByFoodType(FoodType foodType);
--- src/main/java/com/project/petmanagement/petmanagement/services/NutritiousFoodService.java
+++ src/main/java/com/project/petmanagement/petmanagement/services/NutritiousFoodService.java
@@ -7,10 +7,13 @@ import com.project.petmanagement.petmanagement.models.entity.FoodType;
 import com.project.petmanagement.petmanagement.models.entity.Species;
 import com.project.petmanagement.petmanagement.repositories.FoodTypeRepository;
 import com.project.petmanagement.petmanagement.repositories.SpeciesRepository;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 
 import org.springframework.stereotype.Service;
 
 import com.project.petmanagement.petmanagement.models.entity.NutritiousFood;
+import com.project.petmanagement.petmanagement.models.entity.QNutritiousFood;
 import com.project.petmanagement.petmanagement.repositories.NutritiousFoodRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -31,44 +34,73 @@ public class NutritiousFoodService {
                 () -> new DataNotFoundException("Can not find nutritious food with ID: " + nutritiousFoodId));
     }
 
-    public List<NutritiousFood> searchNutritiousFoodByFilter(String keyword, Long speciesId, Long foodTypeId)
-            throws Exception {
-        if (speciesId != null && foodTypeId != null) {
-            Species existingSpecies = speciesRepository.findById(speciesId)
-                    .orElseThrow(() -> new DataNotFoundException("Can not find species with ID: " + speciesId));
-            FoodType existingFoodType = foodTypeRepository.findById(foodTypeId)
-                    .orElseThrow(() -> new DataNotFoundException("Can not find food type with ID: " + foodTypeId));
+    // public List<NutritiousFood> searchNutritiousFoodByFilter(String keyword, Long
+    // speciesId, Long foodTypeId)
+    // throws Exception {
+    // if (speciesId != null && foodTypeId != null) {
+    // Species existingSpecies = speciesRepository.findById(speciesId)
+    // .orElseThrow(() -> new DataNotFoundException("Can not find species with ID: "
+    // + speciesId));
+    // FoodType existingFoodType = foodTypeRepository.findById(foodTypeId)
+    // .orElseThrow(() -> new DataNotFoundException("Can not find food type with ID:
+    // " + foodTypeId));
 
-            if (keyword != null && !keyword.isEmpty()) {
-                return nutritiousFoodRepository.findByNameContainingAndFoodTypeAndSpecies(keyword, existingFoodType,
-                        existingSpecies);
-            } else {
-                return nutritiousFoodRepository.findBySpeciesAndFoodType(existingSpecies, existingFoodType);
-            }
-        } else if (speciesId != null && keyword != null && !keyword.isEmpty()) {
-            Species existingSpecies = speciesRepository.findById(speciesId)
-                    .orElseThrow(() -> new DataNotFoundException("Can not find species with ID: " + speciesId));
-            return nutritiousFoodRepository.findByNameContainingAndSpecies(keyword, existingSpecies);
-        } else if (foodTypeId != null && keyword != null && !keyword.isEmpty()) {
-            FoodType existingFoodType = foodTypeRepository.findById(foodTypeId)
-                    .orElseThrow(() -> new DataNotFoundException("Can not find food type with ID: " + foodTypeId));
+    // if (keyword != null && !keyword.isEmpty()) {
+    // return
+    // nutritiousFoodRepository.findByNameContainingAndFoodTypeAndSpecies(keyword,
+    // existingFoodType,
+    // existingSpecies);
+    // } else {
+    // return nutritiousFoodRepository.findBySpeciesAndFoodType(existingSpecies,
+    // existingFoodType);
+    // }
+    // } else if (speciesId != null && keyword != null && !keyword.isEmpty()) {
+    // Species existingSpecies = speciesRepository.findById(speciesId)
+    // .orElseThrow(() -> new DataNotFoundException("Can not find species with ID: "
+    // + speciesId));
+    // return nutritiousFoodRepository.findByNameContainingAndSpecies(keyword,
+    // existingSpecies);
+    // } else if (foodTypeId != null && keyword != null && !keyword.isEmpty()) {
+    // FoodType existingFoodType = foodTypeRepository.findById(foodTypeId)
+    // .orElseThrow(() -> new DataNotFoundException("Can not find food type with ID:
+    // " + foodTypeId));
 
-            return nutritiousFoodRepository.findByNameContainingAndFoodType(keyword, existingFoodType);
-        } else if (speciesId != null) {
-            Species existingSpecies = speciesRepository.findById(speciesId)
-                    .orElseThrow(() -> new DataNotFoundException("Can not find species with ID: " + speciesId));
-            return nutritiousFoodRepository.findBySpecies(existingSpecies);
-        } else if (foodTypeId != null) {
-            FoodType existingFoodType = foodTypeRepository.findById(foodTypeId)
-                    .orElseThrow(() -> new DataNotFoundException("Can not find food type with ID: " + foodTypeId));
+    // return nutritiousFoodRepository.findByNameContainingAndFoodType(keyword,
+    // existingFoodType);
+    // } else if (speciesId != null) {
+    // Species existingSpecies = speciesRepository.findById(speciesId)
+    // .orElseThrow(() -> new DataNotFoundException("Can not find species with ID: "
+    // + speciesId));
+    // return nutritiousFoodRepository.findBySpecies(existingSpecies);
+    // } else if (foodTypeId != null) {
+    // FoodType existingFoodType = foodTypeRepository.findById(foodTypeId)
+    // .orElseThrow(() -> new DataNotFoundException("Can not find food type with ID:
+    // " + foodTypeId));
 
-            return nutritiousFoodRepository.findByFoodType(existingFoodType);
-        } else {
-            if (keyword != null && !keyword.isEmpty()) {
-                return nutritiousFoodRepository.findByNameContaining(keyword);
-            } else {
-                throw new IllegalArgumentException("At least one parameter must be provided.");
-            }
+    // return nutritiousFoodRepository.findByFoodType(existingFoodType);
+    // } else {
+    // if (keyword != null && !keyword.isEmpty()) {
+    // return nutritiousFoodRepository.findByNameContaining(keyword);
+    // } else {
+    // throw new IllegalArgumentException("At least one parameter must be
+    // provided.");
+    // }
+    // }
+    // }
+    public List<NutritiousFood> searchNutritiousFoodByFilter(String keyword, Long speciesId, Long foodTypeId) {
+        QNutritiousFood nutritiousFood = QNutritiousFood.nutritiousFood;
+        BooleanExpression predicate = nutritiousFood.isNotNull();
+        if (foodTypeId != null) {
+            predicate = predicate.and(nutritiousFood.foodType
+                    .eq(foodTypeRepository.findById(foodTypeId).get()));
         }
+        if (speciesId != null) {
+            predicate = predicate.and(nutritiousFood.species.eq(speciesRepository.findById(speciesId).get()));
+        }
+        if (keyword != null && !keyword.isEmpty()) {
+            StringExpression nutritiousName = nutritiousFood.name;
+            predicate = predicate.and(nutritiousName.like("%" + keyword + "%"));
+        }
+        return (List<NutritiousFood>) nutritiousFoodRepository.findAll(predicate);
     }
 }
